6c9c2ff56399282caab055c903971880c70ddab9
Mirza Kapetanovic <mirza.kapetanovic@gmail.com>
2013-04-17 21:13:38 +0200

    New tracker implementation

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var controller = require('./lib/bitless/controller');
 var storage = require('./lib/bitless/storage');
 var fs = require('fs');
 
+/*
 fs.readFile('./downloads/trusted-computing-local.torrent', function(err, data) {
 	if(err) {
 		console.error(err.stack);
@@ -23,4 +24,18 @@ fs.readFile('./downloads/trusted-computing-local.torrent', function(err, data) {
 	controller.open(t, function(err, manager) {
 		// ...
 	});
+});
+*/
+
+fs.readFile('./downloads/trusted-computing.torrent', function(err, data) {
+	var t = new torrent.Torrent(data);
+	var tracker = require('./lib/bitless/tracker');
+	var peerid = require('./lib/bitless/peer-id');
+
+	var http = new tracker.HttpClient(t, { id: peerid.generate(), port: 6881, announce: t.announce });
+
+	http.on('response', function(response) {
+		console.log(response);
+	});
+	http.request({ numwant: 10 });
 });
